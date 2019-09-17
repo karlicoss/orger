a1f16efaec927f59fcdd7d08be63a8ac8efa4c42
Dima Gerasimov <karlicoss@gmail.com>
2019-09-17 17:33:48 +0400

    update movies

--- movies.py
+++ movies.py
@@ -1,23 +1,20 @@
 #!/usr/bin/env python3
+from orger import View
+from orger.inorganic import node, link
+from orger.org_utils import dt_heading
+
+
 from my.media.movies import get_movies
 
-from orger import Overwrite
-from orger.org_utils import OrgTree, as_org
-
-
-class Movies(Overwrite):
-    file = __file__
-    logger_tag = 'movies'
-
+class Movies(View):
     def get_items(self):
         movies = get_movies()
         for m in movies:
-            yield (m.title, OrgTree(as_org(
-                created=m.created,
-                heading=m.title,
+            yield m.title, node(
+                dt_heading(m.created, m.title),
                 body=f'rating: {m.rating}',
-            )))
+            )
 
 
 if __name__ == '__main__':
-    Movies.main(default_to='movies.org') # TODO logger??
+    Movies.main()
