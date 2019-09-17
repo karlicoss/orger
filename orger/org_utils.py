835b2eb1252258772fe6b1351c27552367f7bb97
Dima Gerasimov <karlicoss@gmail.com>
2019-09-17 17:59:18 +0400

    get rid of OrgTree; handle lazy properties properly

--- orger/org_utils.py
+++ orger/org_utils.py
@@ -1,43 +1,8 @@
 from typing import NamedTuple, Sequence, Any, List, Tuple, Optional, TypeVar, Callable, Union, Type
 
-# TODO move OrgTree to inorganic? not sure about pick_heading
 from .inorganic import as_org_entry, OrgNode, org_dt
 
-# TODO compare before saving? not sure if necessary..
-
-# TODO kython?...
-T = TypeVar('T')
-Lazy = Union[T, Callable[[], T]]
-
-def from_lazy(x: Lazy[T], type_: Type[T]) -> T:
-    if isinstance(x, type_):
-        return x
-    else:
-        return x() # type: ignore
-
-class OrgTree(NamedTuple):
-    item_: Lazy[str]
-    children: Sequence[Any] = ()
-
-    @property
-    def item(self) -> str:
-        return from_lazy(self.item_, type_=str)
-
-    def render_hier(self) -> List[Tuple[int, str]]:
-        res = [(0, self.item)]
-        for ch in self.children:
-            # TODO make sure there is a space??
-            # TODO shit, would be nice to tabulate?.. not sure
-            res.extend((l + 1, x) for l, x in ch.render_hier())
-        return res
-
-    def render(self, level=0) -> str:
-        rh = self.render_hier()
-        rh = [(level + l, x) for l, x in rh]
-        return '\n'.join('*' * l + (' ' if l > 0 else '') + x for l, x in rh)
-
-
-def pick_heading(root: OrgTree, text: str) -> Optional[OrgTree]:
+def pick_heading(root: OrgNode, text: str) -> Optional[OrgNode]:
     if text in root.item:
         return root
     for ch in root.children:
@@ -46,17 +11,6 @@ def pick_heading(root: OrgTree, text: str) -> Optional[OrgTree]:
             return chr
     else:
         return None
-
-
-# TODO hacky...
-def as_org(todo=False, inline_created=True, **kwargs):
-    res = as_org_entry(
-        todo=todo,
-        inline_created=inline_created,
-        level=0,
-        **kwargs,
-    )
-    return res
 
 
 from orger.inorganic import datetime2org
@@ -102,5 +56,6 @@ def test_render_2():
 
 
 def test_render_3():
-    zzz = OrgNode(todo='TODO', heading='hi')
+    # test lazy property
+    zzz = OrgNode(todo='TODO', heading=lambda: 'hi')
     assert zzz.render(level=1).startswith('* TODO hi')
