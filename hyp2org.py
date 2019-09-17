debd40ea2b9903e63d6ddf5c9daa0cc03b100cf8
Dima Gerasimov <karlicoss@gmail.com>
2019-09-17 14:52:17 +0400

    WIP on fixing appended instapaper

--- hyp2org.py
+++ hyp2org.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+from orger import Interactive
+from orger.inorganic import node, link
+from orger.org_utils import dt_heading
+
 from my.hypothesis import get_todos
 
 from kython.org_tools import link as org_link
@@ -7,18 +11,17 @@ from orger import OrgViewAppend, OrgWithKey
 from orger.org_utils import OrgTree, as_org
 
 
-class HypTodos(OrgViewAppend):
+class HypTodos(Interactive):
     file = __file__
     logger_tag = 'hypothesis-todos'
 
     def get_items(self):
         for t in get_todos():
-            yield (
-            t.eid,
-            OrgTree(as_org(
-                todo=True,
-                inline_created=False,
+            yield (t.eid, node(
                 heading=t.content,
+                todo='TODO',
+                # TODO created property?
+                inline_created=False,
                 body=f'''
 {t.annotation}
 {org_link(title=t.title, url=t.link)}
