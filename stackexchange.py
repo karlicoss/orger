824c39f00ee3bd9e02cc4536d04773a0dc3f8c4b
Dima Gerasimov <karlicoss@gmail.com>
2019-09-17 22:44:10 +0400

    import stackexchange orger

---
+++ stackexchange.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+from orger import View
+from orger.inorganic import node, link
+from orger.org_utils import dt_heading
+
+import my.stackexchange as se
+
+
+class Stackexchange(View):
+    def get_items(self):
+        # TODO adapt for other stackexchange items
+        se_data = se.get_data()
+        for q in se_data.questions:
+            # TODO could emit items along with level, then it would look a bit more natural
+            yield node(
+                dt_heading(q.creation_date, link(url=q.link, title=q.title)),
+                tags=q.tags,
+                body=q.body_markdown, # TODO eh, would be useful to have md2org perhaps?
+            )
+
+
+if __name__ == '__main__':
+    Stackexchange.main()
