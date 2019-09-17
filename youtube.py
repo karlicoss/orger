1ff1cef576da5314b27af84858b8e9642a94dc5d
Dima Gerasimov <karlicoss@gmail.com>
2019-09-17 16:12:27 +0400

    switch youtube to orgnode

--- youtube.py
+++ youtube.py
@@ -1,27 +1,26 @@
 #!/usr/bin/env python3
+from orger import View
+from orger.inorganic import node, link
+from orger.org_utils import dt_heading
+
 from my.media.youtube import get_watched
 
 from kython import group_by_key
-from kython.org_tools import link as org_link
 
-from orger import Overwrite
-from orger.org_utils import OrgTree, as_org
-
-
-class YoutubeView(Overwrite):
-    file = __file__
-    logger_tag = 'youtube-view'
-
+class YoutubeView(View):
     def get_items(self):
         watched = get_watched()
-        items = [max(group, key=lambda w: w.when) for _, group in group_by_key(watched, key=lambda w: w.url).items()]
+        items = [
+            max(group, key=lambda w: w.when)
+            for _, group in group_by_key(watched, key=lambda w: w.url).items()
+        ]
         items = sorted(items, key=lambda w: w.when)
         # TODO for each url only take latest?
         for item in items:
-            yield (item.url, OrgTree(as_org(
-                created=item.when,
-                heading=org_link(title=item.title, url=item.url),
-            )))
+            yield (item.url, node(
+                heading=dt_heading(item.when, link(title=item.title, url=item.url)),
+            ))
+
 
 if __name__ == '__main__':
     YoutubeView.main(default_to='youtube.org')
