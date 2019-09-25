725b2484c3a821c50ad178cdd727cf9f590e3dcc
Dima Gerasimov <karlicoss@gmail.com>
2019-09-25 01:10:08 +0100

    add py.typed

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
 from setuptools import setup, find_packages
 
+name = 'orger'
+
 if __name__ == '__main__':
     setup(
-        name='orger',
+        name=name,
         version='0.0.1',
         url='https://github.com/karlicoss/orger',
-        author='Dima Geerasimov',
+        author='Dima Gerasimov',
         author_email='karlicoss@gmail.com',
-        description='Converts data int org-mode',
+        description='Converts data into org-mode',
         packages=find_packages(),
         extras_require={
             'testing':  ['pytest', 'mypy', 'pylint'],
         },
+        package_data={name: ['py.typed']},
     )
