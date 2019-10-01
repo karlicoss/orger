b3828594a2f08f53164ffee26869dedb863cfd51
Dima Gerasimov <karlicoss@gmail.com>
2019-10-01 22:27:10 +0100

    use public my.pdf package

--- modules/pdfs.py
+++ modules/pdfs.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python3
 from orger import StaticView
 from orger.inorganic import node, link
-from orger.common import dt_heading
+from orger.common import dt_heading, setup_logger
 
+import logging
 from datetime import datetime
-from kython.kerror import unwrap
 
-from my.reading import pdfs
+import my.pdfs as pdfs
+
 
 class PdfView(StaticView):
     def get_items(self):
-        from pathlib import Path
         for pdf in sorted(
                 pdfs.get_annotated_pdfs(),
                 key=lambda p: datetime.min if p.date is None else p.date.replace(tzinfo=None),
         ):
-            yield f'pdf_annotation_{pdf.path}', node(
+            yield node(
                 dt_heading(pdf.date, str(pdf.path)),
                 children=[node(
                     dt_heading(a.date, f'page {a.page} {a.highlight or ""}'),
@@ -25,4 +25,5 @@ class PdfView(StaticView):
 
 
 if __name__ == '__main__':
+    setup_logger(pdfs.get_logger(), level=logging.DEBUG)
     PdfView.main()
