554eaaf3e4dded1258a6c317eed36ea2c414db96
Dima Gerasimov <karlicoss@gmail.com>
2019-10-11 22:29:29 +0100

    remove kython and add example

--- modules/polar.py
+++ modules/polar.py
@@ -1,26 +1,38 @@
 #!/usr/bin/env python3
-from orger import StaticView, OrgWithKey
-from orger.inorganic import node, link
-from orger.common import dt_heading
+"""
+Example output:
 
-from kython.kerror import unwrap
+
+* [2018-11-28 Wed 22:04] [Baez] Lectures on Classical Mechanics.pdf 12nwnBFvng-[Baez] Lectures on Classical Mechanics.pdf
+...
+* [2019-01-20 Sun 14:00] [Woit] Quantum Theory, Groups and Representations.pdf 12LWfRf8Gf-[Woit] Quantum Theory, Groups and Representations.pdf
+** [2019-01-20 Sun 14:34]  An important corollary of Schurs lemma is the following characterization of irreducible representations of G when G is commutative.
+** [2019-01-20 Sun 14:40]  As such it has a differential, which  is  a  linear  map  from  the  tangent  space  at  the  identity  of U (1)  (which here is i R ) to the tangent space at the identity of GL ( n, C ) (which is the space M ( n, C ) of n by n complex matrices).  Th e tangent space at the identity of a Lie group is called a Lie algebra
+** [2019-01-20 Sun 14:47]  n general [ Q,H ] 6 = 0, with Q then gen- erating a unitary action on H that does not commute with time evolution and does not imply a conservation law
+** [2019-01-20 Sun 14:49]   In the limit N one can make contact with classical notions of spinning objects and angular momentum, but the spin
+** [2019-01-20 Sun 15:25]   For further reading
+*** [2019-01-20 Sun 15:25] Right. so here I think I missed the bit where he only restricted to spin 1/2
+"""
+
+
+from orger import StaticView
+from orger.inorganic import node
+from orger.common import dt_heading
 
 from my.reading import polar
 
 class PolarView(StaticView):
     def get_items(self):
-        def make_item(res: polar.Result) -> OrgWithKey:
-            try:
-                b = unwrap(res)
-            except polar.Error as e:
-                return f'error_{e.uid}', node(heading=str(e))
+        def make_item(res: polar.Result):
+            if isinstance(res, polar.Error):
+                return node(heading='ERROR ' + str(res)) # TODO priority A?
             else:
-                return b.uid, node(
+                b = res
+                return node(
                     heading=dt_heading(b.created, f'{b.title} {b.filename}'),
                     # tags=b.tags, # TODO?
                     children=[node(
                         heading=dt_heading(hl.created, hl.selection),
-                        # TODO some shitty characters; generally concatenated text doesn't look great...
                         children=[node(
                             heading=dt_heading(c.created, c.text)
                         ) for c in hl.comments]
@@ -28,6 +40,8 @@ class PolarView(StaticView):
                 )
         for b in polar.get_entries():
             yield make_item(b)
+
+# TODO convert html markup to org-mode
 
 
 test = PolarView.make_test(
