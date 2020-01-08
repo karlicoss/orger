94b8e76bcc0e3ccd166da0da18b92d4db2c3cd1b
Dima Gerasimov <karlicoss@gmail.com>
2020-01-08 21:07:33 +0000

    Proper error handling for PDF module

--- modules/pdfs.py
+++ modules/pdfs.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from orger import StaticView
 from orger.inorganic import node, link
-from orger.common import dt_heading, setup_logger
+from orger.common import dt_heading, setup_logger, error
 
 import logging
 from datetime import datetime
@@ -12,9 +12,12 @@ import my.pdfs as pdfs
 class PdfView(StaticView):
     def get_items(self):
         for pdf in sorted(
-                pdfs.get_annotated_pdfs(),
-                key=lambda p: datetime.min if p.date is None else p.date.replace(tzinfo=None),
+                pdfs.annotated_pdfs(),
+                key=lambda p: datetime.min if isinstance(p, Exception) or p.date is None else p.date.replace(tzinfo=None),
         ):
+            if isinstance(pdf, Exception):
+                yield error(pdf)
+                continue
             yield node(
                 dt_heading(pdf.date, str(pdf.path)),
                 children=[node(
