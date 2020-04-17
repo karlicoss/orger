07dfa96b28517ff96edaedf11528615250eb8c1a
Dima Gerasimov <karlicoss@gmail.com>
2020-04-17 22:39:07 +0100

    update setup.py and add release script

--- setup.py
+++ setup.py
@@ -1,22 +1,23 @@
 from setuptools import setup, find_packages
-from os.path import join, dirname
 
-name = 'orger'
 
 if __name__ == '__main__':
+    [pkg] = find_packages('src')
     setup(
-        name=name,
-        version='0.1.1',
+        name=pkg,
+        use_scm_version=True,
+        setup_requires=['setuptools_scm'],
+
         url='https://github.com/karlicoss/orger',
         author='Dima Gerasimov',
         author_email='karlicoss@gmail.com',
         description='Converts data into org-mode',
-        package_dir={name: join(dirname(__file__), 'src', 'orger')},
-        packages=[name],
+        packages=[pkg],
+        package_dir={'': 'src'},
         install_requires=['atomicwrites'],
         extras_require={
             'testing': ['pytest'],
             'linting': ['pytest', 'mypy', 'pylint'],
         },
-        package_data={name: ['py.typed']},
+        package_data={pkg: ['py.typed']},
     )
