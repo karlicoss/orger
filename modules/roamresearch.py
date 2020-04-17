b2f5b4a4f2929ab860faf030fd611c67933cf15e
Dima Gerasimov <karlicoss@gmail.com>
2020-04-17 21:51:51 +0100

    Support internal links for Roam

--- modules/roamresearch.py
+++ modules/roamresearch.py
@@ -7,17 +7,36 @@ from orger.common import dt_heading
 import my.roamresearch as roamresearch
 
 
-def roam_to_org(node: roamresearch.Node) -> OrgNode:
+def roam_note_to_org(node: roamresearch.Node) -> OrgNode:
+    """
+    Converts Roam node into Org-mode representation
+    """
+    title = node.title
+    # org-mode target allows jumping straight into
+    # conveniently, links in Roam are already represented as [[link]] !
+    target = '' if title is None else f'<<{title}>> '
+    heading = target + link(title='x', url=node.permalink)
+
+    body = node.body
+    if body is not None:
+        lines = body.splitlines(keepends=True)
+        # display first link of the body as the heading
+        if len(lines) > 0:
+            heading = heading + ' ' + lines[0]
+            body = ''.join(lines[1:])
+            if len(body) == 0:
+                body = None
     return OrgNode(
-        heading=link(title=node.title, url=node.permalink),
-        children=list(map(roam_to_org, node.children)),
+        heading=heading,
+        body=body,
+        children=list(map(roam_note_to_org, node.children)),
     )
-    # TODO inline links
+
 
 class RoamView(StaticView):
     def get_items(self):
         rr = roamresearch.roam()
-        yield from map(roam_to_org, rr.nodes)
+        yield from map(roam_note_to_org, rr.nodes)
 
 
 if __name__ == '__main__':
