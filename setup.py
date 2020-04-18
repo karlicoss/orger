a3a11a3a34ab4e40692b3d503f885cf20474f239
Dima Gerasimov <karlicoss@gmail.com>
2020-04-18 01:19:50 +0100

    Fix version string

--- setup.py
+++ setup.py
@@ -2,10 +2,17 @@ from setuptools import setup, find_packages
 
 
 if __name__ == '__main__':
+    # from setuptools_scm import get_version
+    # https://github.com/pypa/setuptools_scm#default-versioning-scheme
+    # get_version(version_scheme='python-simplified-semver', local_scheme='no-local-version')
+   
     [pkg] = find_packages('src')
     setup(
         name=pkg,
-        use_scm_version=True,
+        use_scm_version={
+            'version_scheme': 'python-simplified-semver',
+            'local_scheme': 'dirty-tag',
+        },
         setup_requires=['setuptools_scm'],
 
         url='https://github.com/karlicoss/orger',
