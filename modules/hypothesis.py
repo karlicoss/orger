e32456c365715e9baa21245a316afd454a06a7ba
Dima Gerasimov <karlicoss@gmail.com>
2020-06-01 17:04:53 +0100

    core & modules updates
    - core: expose Mirror.Results
    - github module: handle errors (to address https://github.com/karlicoss/HPI/issues/59 )
    - misc: better mypy coverage

--- modules/hypothesis.py
+++ modules/hypothesis.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
-from orger import StaticView
+from orger import Mirror
 from orger.inorganic import node, link
 from orger.common import dt_heading, error
 
-from my.hypothesis import get_pages
+import my.hypothesis as hypothesis
 
 
-class HypView(StaticView):
-    def get_items(self):
-        for page in get_pages():
+class HypView(Mirror):
+    def get_items(self) -> Mirror.Results:
+        for page in hypothesis.pages():
             # TODO would be nice to signal error upwards? Maybe just yield Exception, rener it in Orger and allow setting error status?
             if isinstance(page, Exception):
                 yield error(page)
--- modules/kobo.py
+++ modules/kobo.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
-from orger import StaticView
-from orger.inorganic import node, link
+from orger import Mirror
+from orger.inorganic import node, link, OrgNode
 from orger.common import dt_heading
 
 from my.books.kobo import get_books_with_highlights, Highlight
 
 
-class KoboView(StaticView):
-    def get_items(self):
-        def render_highlight(h: Highlight):
+class KoboView(Mirror):
+    def get_items(self) -> Mirror.Results:
+        def render_highlight(h: Highlight) -> OrgNode:
             # TODO FIXME could use bookmark page??
             heading = 'bookmark' if h.kind == 'bookmark' else (h.text or '')
             body = h.annotation # TODO check if empty
