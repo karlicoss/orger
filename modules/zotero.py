b00f0f0d11ace32e4700bd34435e51487d03c181
Dima Gerasimov <karlicoss@gmail.com>
2021-04-11 19:25:03 +0100

    zotero: various enhancements
    - add excerpt from user's comment to heading
    - add ZOTERO_TAGS property
    - add color as tags & ZOTERO_COLOR property
    - add item tags

--- modules/zotero.py
+++ modules/zotero.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+
 from orger import Mirror
 from orger.inorganic import node, link, docview_link, literal
 from orger.common import dt_heading, error
@@ -9,7 +10,28 @@ from textwrap import indent, wrap
 from more_itertools import bucket
 
 
+class config:
+    MAX_LINE_WIDTH: int = 120
+
+
+colors_doc = """
+To highlight tags based on color, add this to your Emacs config:
+  (with-eval-after-load 'org
+    (dolist (color '("yellow" "red" "purple" "blue" "green"))
+        (add-to-list 'org-tag-faces
+                     `(,color . (:background ,color)))))
+
+NOTE: you'll need to restart emacs for this to take effect.
+If some colors look too bright for you, you can just manually map them instead, e.g.:
+  (with-eval-after-load 'org
+    (add-to-list 'org-tag-faces '("red"   . (:foreground "#cc0000")))
+    (add-to-list 'org-tag-faces '("green" . (:foreground "#00aa00"))))
+"""
+
+
 class Zotero(Mirror):
+    DEFAULT_HEADER = Mirror.DEFAULT_HEADER + indent(colors_doc, '# ', lambda line: True)
+
     def get_items(self) -> Mirror.Results:
         from my import zotero
 
@@ -33,7 +55,8 @@ class Zotero(Mirror):
                     parts = []
                     text = a.text
                     if text is not None:
-                        text = '\n'.join(wrap(text, width=150))  # todo make configurable?
+                        # todo not sure about it here... maybe should rely on softwrap in emacs instead?
+                        text = '\n'.join(wrap(text, width=config.MAX_LINE_WIDTH))
                         text = literal(text)
                         parts.append(text)
                     comment = a.comment
@@ -42,25 +65,44 @@ class Zotero(Mirror):
                     page1 = a.page + 1  # NOTE: zotero using 0-indexing, pdfview using 1-indexing
                     body = '\n'.join(parts)
 
+                    color = a.color_human
+                    tags = list(a.tags)
+                    # todo not sure which is best?
+                    tags.append(color)
+                    properties = {
+                        'ZOTERO_COLOR': color,
+                    }
+                    if len(a.tags) > 0:
+                        # zotero tags cal be multi-word? guess worth adding just in case
+                        properties['ZOTERO_TAGS'] = ', '.join(a.tags)  # not sure what's the best separator...
+
                     # todo not sure about it...
                     mtodo: Optional[str] = None
-                    if 'todo' in {t.lower() for t in a.tags}:
+                    if 'todo' in {t.lower() for t in tags}:
                         mtodo = 'TODO'
 
+                    heading = docview_link(path=item.file, title=f'page {page1}', page1=page1)
+                    if comment is not None:
+                        # try to display first few words?
+                        cline = wrap(comment, width=config.MAX_LINE_WIDTH)[0]
+                        heading = heading + ' ' + cline
+                    # todo would be nice to align tags, maybe...
                     yield node(
+                        todo=mtodo,
                         heading=dt_heading(
                             a.added,
-                            docview_link(path=item.file, title=f'on page {page1}', page1=page1)
+                            heading,
                         ),
-                        todo=mtodo,
+                        tags=tags,
+                        properties=properties,
                         body=body,
-                        tags=a.tags,
                     )
             body = ''
             if url := item.url:
                 body = url
             yield node(
                 heading=docview_link(path=item.file, title=item.title),
+                tags=item.tags,
                 body=body,
                 children=list(chit())
             )
